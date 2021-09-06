9d126b684c9b7581f2de520946824c1336d7f55a
hdac-hmh <myunghwan@hdac.io>
2021-09-06 17:19:09 +0900

    test: update test script for transfer command

--- scripts/run-privatenode.sh
+++ scripts/run-privatenode.sh
@@ -10,9 +10,9 @@ fi
 
 rizond init my_node --chain-id my_testnet
 rizond keys add my_key --keyring-backend test
-
+rizond keys add my_key2 --keyring-backend test
 rizond add-genesis-account $(rizond keys show my_key -a --keyring-backend test) 10000000000stake
-
+rizond add-genesis-account $(rizond keys show my_key2 -a --keyring-backend test) 10000000000stake
 rizond gentx my_key 1000000000stake --chain-id my_testnet --keyring-backend test
 rizond collect-gentxs
 rizond validate-genesis
